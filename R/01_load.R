dde3958a269c369d31863ae5a8d1264e1312cbf1
Jacob Kofoed <s960793@student.dtu.dk>
2020-04-30 17:03:43 +0000

    loading more datasets

--- R/01_load.R
+++ R/01_load.R
@@ -4,6 +4,9 @@ rm(list = ls())
 
 # Load libraries
 # ------------------------------------------------------------------------------
+#install.packages("tidyverse")
+#install.packages("ggplot2")
+#install.packages("UniprotR")
 library("tidyverse")
 
 # Define functions
@@ -23,10 +26,10 @@ library("tidyverse")
 #write_tsv(x = my_data,
 #          path = "data/01_my_data.tsv")
 
-library("ggseqlogo")
+#library("ggseqlogo")
 library("readxl")
-library("ggplot2")
-library('UniprotR') 
+#library("ggplot2")
+#library('UniprotR') 
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -35,15 +38,24 @@ source(file = "R/99_project_functions.R")
 # Load data
 # ------------------------------------------------------------------------------
 # load amino acid scales
-aa_scale_z5 <- load_scale('z5')
+#aa_scale_z5 <- load_scale('z5')
 
-aa_scale_bl62 <- load_scale('bl62')
+#aa_scale_bl62 <- load_scale('bl62')
 
 # load peptide sequence and results
-data_set_1 <- read_excel('./_raw/genetics.115.175802-6.xls')
 
-data_set_2 <- read_excel('./_raw/1-s2.0-S2211124716313171-mmc2.xlsx',
+# paper reference here: xxxx
+data_set_1 <- read_excel('./data/_raw/genetics.115.175802-6.xls')
+
+# paper reference here: xxxx
+data_set_2 <- read_excel('./data/_raw/1-s2.0-S2211124716313171-mmc2.xlsx',
                          sheet = 'Supplemental_Table_1')
+
+# paper reference here: xxxx
+data_set_3 <- read_csv('./data/_raw/urn_mavedb_00000036-a-1_scores.csv', skip = 4)
+
+# data_set_4: https://doi.org/10.1371/journal.pgen.1004918
+data_set_4 <- read_tsv('./data/_raw/S1_Table.txt')
 
 
 # Write data
@@ -54,4 +66,8 @@ write_tsv(x = data_set_1,
 write_tsv(x = data_set_2,
           path = "./data/01_load_data_set_2.tsv")
 
+write_tsv(x = data_set_3,
+          path = "./data/01_load_data_set_3.tsv")
 
+write_tsv(x = data_set_4,
+          path = "./data/01_load_data_set_4.tsv")
