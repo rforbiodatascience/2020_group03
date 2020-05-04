0485b6d129a29d12d6b8858e6ad3b07f99927894
Jacob Kofoed <s960793@student.dtu.dk>
2020-05-04 08:47:00 +0000

    sequence conversion working as functions

--- R/03_augment.R
+++ R/03_augment.R
@@ -14,7 +14,7 @@ source(file = "R/99_functions.R")
 # Load data
 # ------------------------------------------------------------------------------
 my_data_clean_aug <- read_table2(file = "data/clean_test.txt", col_names = FALSE)
-amino_acids <- read_csv("data/_raw/amino_acids.csv", col_names = TRUE)
+
 
 # Wrangle data
 # ------------------------------------------------------------------------------
@@ -53,45 +53,14 @@ clean_data_set_4 <- read_tsv(file = "./data/02_clean_data_set_4.tsv")
 
 # Wrangle data
 # ------------------------------------------------------------------------------
-
-# need a function
-
 aug_data_set_1 <- clean_data_set_1 %>%
-  mutate(
-    mutated_residue = get_mutated_residue(variant), 
-    mutation_position = get_mutation_position(variant),
-    mutation = get_mutation(variant),
-    sequence = make_sequence(mutated_residue, mutation_position, mutation, "P38398"))
-
-aug_data_set_2 <- clean_data_set_2 %>%
-  mutate(
-    mutated_residue = get_mutated_residue(variant), 
-    mutation_position = get_mutation_position(variant),
-    mutation = get_mutation(variant),
-    sequence = make_sequence(mutated_residue, mutation_position, mutation, "P28482"))
-
-aug_data_set_3 <- clean_data_set_3 %>%
-  mutate(
-    mutated_residue_3_letter = str_extract(variant, "[A-Z][a-z][a-z]"), 
-    mutation_position = str_extract(variant, "[0-9]+"),
-    mutation_3_letter = str_extract(variant, "[A-Z,a-z,*]+$"),
-    mutation_3_letter = str_replace(mutation_3_letter, "[*]", mutated_residue_3_letter)) %>%
-  inner_join(
-    amino_acids, by = c("mutation_3_letter" = "threeletter")) %>%
-  rename(mutation = oneletter) %>%
-  inner_join(
-    amino_acids, by = c("mutated_residue_3_letter" = "threeletter")) %>%
-  rename(mutated_residue = oneletter) %>%
-  mutate(
-    Sequence = make_sequence(mutated_residue, mutation_position, mutation, "Q5SW96"))
-
-aug_data_set_4 <- clean_data_set_4 %>%
-  mutate(
-    mutated_residue = get_mutated_residue(variant), 
-    mutation_position = get_mutation_position(variant),
-    mutation = get_mutation(variant),
-    sequence = make_sequence(mutated_residue, mutation_position, mutation, "P04147"))
-
+  mutate(variant = case_when(variant=="NA-NA" ~ "D2D",
+                             !variant=="NA-NA" ~ variant))
+# augmenting with sequence
+aug_data_set_1 <- convert_variant_to_sequence(aug_data_set_1, "P38398")
+aug_data_set_2 <- convert_variant_to_sequence(clean_data_set_2, "P28482")
+aug_data_set_3 <- convert_variant_to_sequence(clean_data_set_3, "Q5SW96")
+aug_data_set_4 <- convert_variant_to_sequence(clean_data_set_3, "P04147")
 
 # Write data
 # ------------------------------------------------------------------------------
--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -34,6 +34,12 @@ encode_peptide = function(x, m)
 }
 
 # Simple function to convert a mutation into a protein sequence based on a unprot id
+#
+# Usage:
+# make_sequence(Mutated_residue, Mutation_position, Mutation, uniprot_ID)
+#
+# Returns:
+# modified sequence
 make_sequence = function(Mutated_residue, Mutation_position, Mutation, uniprot_ID)
 {
   WT_sequence <- as_tibble(GetSequences(uniprot_ID)) %>%
@@ -43,21 +49,54 @@ make_sequence = function(Mutated_residue, Mutation_position, Mutation, uniprot_I
   return(sequence)
 }
 
-# Simple function to convert a mutation description into an mutated residue
+# Function to convert a mutation description into an mutated residue
+#
+# Usage:
+# get_mutated_residue(variant)
+#
+# Returns:
+# mutated residue
 get_mutated_residue <- function(variant) {
   mutated_residue = str_extract(variant, "[A-Z]")
   return(mutated_residue)
 }
 
-# Simple function to convert a mutation description into an mutattion position
+# Function to convert a mutation description into an mutattion position
+#
+# Usage:
+# get_mutation_position(variant)
+#
+# Returns:
+# mutation position
 get_mutation_position <- function(variant) {
   mutation_position = str_extract(variant, "[0-9]+")
   return(mutation_position)
 }
 
-# Simple function to convert a mutation description into an mutation
+# Function to convert a mutation description into an mutation
+#
+# Usage:
+# get_mutation(variant)
+#
+# Returns:
+# mutation
 get_mutation <- function(variant) {
   mutation = str_extract(variant, "[A-Z,*]$")
   return(mutation)
 }
 
+# Function to convert variant to sequence
+#
+# Usage:
+# convert_variant_to_sequence(tibble, uniprot_id)
+# Returns:
+# tibble augmented with sequence
+convert_variant_to_sequence <- function(df, protein)  {
+  clean_df <- df %>%
+  mutate(
+    mutated_residue = get_mutated_residue(variant), 
+    mutation_position = get_mutation_position(variant),
+    mutation = get_mutation(variant),
+    sequence = make_sequence(mutated_residue, mutation_position, mutation, protein))
+  return(clean_df)
+}
