236f7742153f005d17245df8d4962a8b2ec438a2
Laura Sans <56506836+laurasansc@users.noreply.github.com>
2020-05-05 13:44:31 +0200

    small changes in test files

--- R/03_augment.R
+++ R/03_augment.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Clear workspace
 # ------------------------------------------------------------------------------
 rm(list = ls())
@@ -72,7 +71,6 @@ aug_data_set_1 <- clean_data_set_1 %>%
 write_tsv(x = aug_data_set_1,
           path = "./_raw/data/03_aug_data_set_1.tsv")
 
-=======
 # Clear workspace
 # ------------------------------------------------------------------------------
 rm(list = ls())
@@ -95,40 +93,41 @@ data <- read_table2(file = "data/clean_test.txt", col_names = FALSE)
 # ------------------------------------------------------------------------------
 
 # Remove non-important columns
-<<<<<<< HEAD
+
 # my_data_clean_aug <- my_data_clean_aug %>% 
 #   rename(
 #     peptide = X1,
 #     activity = X2,
 #   ) %>%  select(c(1,2))
-=======
+
 data <- data %>% 
   rename(
     peptide = X1,
     activity = X2,
   ) %>%  select(c(1,2))
->>>>>>> master
 
 data$len = str_length(data$peptide)
 data <- data %>% group_by(len) %>% filter(len == 15) 
+write.csv(data,"data/data_clean_ii.csv", row.names = FALSE)
 
-<<<<<<< HEAD
 # Add new columns (encoding)
 encoded_seq = data %>%
   pull(peptide) %>% 
   encode_peptide(m = "blosum62")
-=======
+
+sequences <- data.frame(row.names(encoded_seq))
+allData <- cbind(sequences, encoded_seq)
+
+write.csv(allData,"data/encoded_seq_with_seq.csv", row.names = TRUE)
+write.csv(encoded_seq,"data/encoded_seq.csv", row.names = TRUE)
 #pepts <- my_data_clean_aug %>% 
 #  pull(peptide) %>% 
 #  encode_peptide("blosum62")
->>>>>>> master
 
 # Write data
 # ------------------------------------------------------------------------------
 #write_tsv(x = my_data_clean_aug,
-<<<<<<< HEAD
 #         path = "data/03_my_data_clean_aug.tsv")
-=======
 #         path = "data/03_my_data_clean_aug.tsv")
 
 
@@ -161,7 +160,6 @@ aug_data_set_4 <- convert_variant_to_sequence(clean_data_set_3, "P04147")
 write_tsv(x = aug_data_set_1,
           path = "./data/03_aug_data_set_1.tsv")
 
-<<<<<<< HEAD
 write_tsv(x = aug_data_set_2,
           path = "./data/03_aug_data_set_2.tsv")
 
@@ -170,7 +168,4 @@ write_tsv(x = aug_data_set_3,
 
 write_tsv(x = aug_data_set_4,
           path = "./data/03_aug_data_set_4.tsv")
-=======
->>>>>>> master
->>>>>>> master
->>>>>>> master
+
