24c462f6c75c3396c5a60e897a455ff6a9e3450d
s193036 <s193036@student.dtu.dk>
2020-05-06 12:51:53 +0000

    bego_ann and new installed packages

--- R/00_doit.R
+++ R/00_doit.R
@@ -1,33 +1,24 @@
-<<<<<<< HEAD
+
 # Install packages
 # ------------------------------------------------------------------------------
-#install.packages("tidyverse")
-#install.packages("ggplot2")
-#install.packages("UniprotR")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("UniprotR")
 install.packages("devtools")
-#install.packages("keras")
 install.packages('neuralnet')
+library("devtools")
+install_github("rstudio/keras")
+
+# Would you like to install miniconda? Y
 library(keras)
 install_keras(tensorflow = "1.13.1")
+
 # Run all scripts
 # ------------------------------------------------------------------------------
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
 source(file = "R/04_analysis_i.R")
+source(file = "R/04_analysis_ii.R")
+source(file = "R/04_analysis_iii.R")
 
-=======
-# Install packages
-# ------------------------------------------------------------------------------
-#install.packages("tidyverse")
-#install.packages("ggplot2")
-#install.packages("UniprotR")
-
-# Run all scripts
-# ------------------------------------------------------------------------------
-source(file = "R/01_load.R")
-source(file = "R/02_clean.R")
-source(file = "R/03_augment.R")
-#source(file = "R/04_analysis_i.")
-
->>>>>>> 288df4c889b2ff9ca2c8d8c0ed71ba630dee6161
