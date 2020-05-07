78ef87e4a523c53bb1f1ecba84df320b9b3daa2f
Jacob Kofoed <s960793@student.dtu.dk>
2020-05-07 09:47:03 +0000

    corrected load, clean and augment

--- R/03_augment.R
+++ R/03_augment.R
@@ -1,75 +1,5 @@
-# Clear workspace
+# 03_augment.R
 # ------------------------------------------------------------------------------
-rm(list = ls())
-
-# Load libraries
-# ------------------------------------------------------------------------------
-library("tidyverse")
-
-# Define functions
-# ------------------------------------------------------------------------------
-source(file = "R/99_functions.R")
-
-# Load data
-# ------------------------------------------------------------------------------
-my_data_clean_aug <- read_table2(file = "data/clean_test.txt", col_names = FALSE)
-
-# Wrangle data
-# ------------------------------------------------------------------------------
-
-# Remove non-important columns
-my_data_clean_aug <- my_data_clean_aug %>% 
-  rename(
-    peptide = X1,
-    activity = X2,
-  ) %>%  select(c(1,2))
-
-# Add new columns (encoding)
-
-pepts <- my_data_clean_aug %>% 
-  pull(peptide) %>% 
-  encode_peptide("blosum62")
-
-# Write data
-# ------------------------------------------------------------------------------
-#write_tsv(x = my_data_clean_aug,
-#         path = "data/03_my_data_clean_aug.tsv")
-library("UniprotR")
-
-# Define functions
-# ------------------------------------------------------------------------------
-source(file = "R/99_project_functions.R")
-
-
-# Load data
-# ------------------------------------------------------------------------------
-clean_data_set_1 <- read_tsv(file = "./_raw/data/02_clean_data_set_1.tsv")
-clean_data_set_2 <- read_tsv(file = "./_raw/data/02_clean_data_set_2.tsv")
-
-
-# Wrangle data
-# ------------------------------------------------------------------------------
-aug_data_set_1 <- clean_data_set_1 %>% 
-  mutate(
-    Mutated_residue = str_extract(Variant_ID, "[A-Z]"),
-    Mutation_position = str_extract(Variant_ID, "[0-9]+"),
-    Mutation = str_extract(Variant_ID, "[A-Z,*]$"),
-    Mutation = str_replace(Mutation, "[*]", Mutated_residue),
-    Sequence = make_sequence(Mutated_residue, Mutation_position, Mutation, "P38398"))
-
-# aug_data_set_2 <- clean_data_set_2 %>% 
-#   mutate(
-#     Mutated_residue = str_extract(ERK2_Mutant, "[A-Z]"),
-#     Mutation_position = str_extract(ERK2_Mutant, "[0-9]+"),
-#     Mutation = str_extract(ERK2_Mutant, "[A-Z,*]$"),
-#     Mutation = str_replace(Mutation, "[*]", Mutated_residue),
-#     Sequence = make_sequence(Mutated_residue, Mutation_position, Mutation, "P38398"))
-
-
-# Write data
-# ------------------------------------------------------------------------------
-write_tsv(x = aug_data_set_1,
-          path = "./_raw/data/03_aug_data_set_1.tsv")
 
 # Clear workspace
 # ------------------------------------------------------------------------------
@@ -79,62 +9,16 @@ rm(list = ls())
 # ------------------------------------------------------------------------------
 library("tidyverse")
 library("UniprotR")
-library("protr")
 
 # Define functions
 # ------------------------------------------------------------------------------
-source(file = "R/99_functions.R")
+source(file = "./R/99_functions.R")
 
-# Load data
-# ------------------------------------------------------------------------------
-data <- read_table2(file = "data/clean_test.txt", col_names = FALSE)
-
-# Wrangle data
-# ------------------------------------------------------------------------------
-
-# Remove non-important columns
-
-# my_data_clean_aug <- my_data_clean_aug %>% 
-#   rename(
-#     peptide = X1,
-#     activity = X2,
-#   ) %>%  select(c(1,2))
-
-data <- data %>% 
-  rename(
-    peptide = X1,
-    activity = X2,
-  ) %>%  select(c(1,2))
-
-data$len = str_length(data$peptide)
-data <- data %>% group_by(len) %>% filter(len == 15) 
-write.csv(data,"data/data_clean_ii.csv", row.names = FALSE)
-
-# Add new columns (encoding)
-encoded_seq = data %>%
-  pull(peptide) %>% 
-  encode_peptide(m = "blosum62")
-
-sequences <- data.frame(row.names(encoded_seq))
-allData <- cbind(sequences, encoded_seq)
-
-write.csv(allData,"data/encoded_seq_with_seq.csv", row.names = TRUE)
-write.csv(encoded_seq,"data/encoded_seq.csv", row.names = TRUE)
-#pepts <- my_data_clean_aug %>% 
-#  pull(peptide) %>% 
-#  encode_peptide("blosum62")
-
-# Write data
-# ------------------------------------------------------------------------------
-#write_tsv(x = my_data_clean_aug,
-#         path = "data/03_my_data_clean_aug.tsv")
-#         path = "data/03_my_data_clean_aug.tsv")
 
 
 # Define functions
 # ------------------------------------------------------------------------------
-source(file = "R/99_project_functions.R")
-
+source(file = "./R/99_project_functions.R")
 
 # Load data
 # ------------------------------------------------------------------------------
@@ -143,17 +27,20 @@ clean_data_set_2 <- read_tsv(file = "./data/02_clean_data_set_2.tsv")
 clean_data_set_3 <- read_tsv(file = "./data/02_clean_data_set_3.tsv")
 clean_data_set_4 <- read_tsv(file = "./data/02_clean_data_set_4.tsv")
 
-
 # Wrangle data
 # ------------------------------------------------------------------------------
 aug_data_set_1 <- clean_data_set_1 %>%
   mutate(variant = case_when(variant=="NA-NA" ~ "D2D",
                              !variant=="NA-NA" ~ variant))
+
 # augmenting with sequence
 aug_data_set_1 <- convert_variant_to_sequence(aug_data_set_1, "P38398")
 aug_data_set_2 <- convert_variant_to_sequence(clean_data_set_2, "P28482")
 aug_data_set_3 <- convert_variant_to_sequence(clean_data_set_3, "Q5SW96")
-aug_data_set_4 <- convert_variant_to_sequence(clean_data_set_3, "P04147")
+aug_data_set_4 <- convert_variant_to_sequence(clean_data_set_4, "P04147")
+
+aug_data_set_4 <- aug_data_set_4 %>%
+  drop_na(score)
 
 # Write data
 # ------------------------------------------------------------------------------
@@ -169,3 +56,19 @@ write_tsv(x = aug_data_set_3,
 write_tsv(x = aug_data_set_4,
           path = "./data/03_aug_data_set_4.tsv")
 
+
+data <- aug_data_set_4
+
+sequence_window <- data %>%
+  mutate(mutation_position = as.integer(mutation_position)) %>%
+  pull(mutation_position)
+
+data <- data %>%
+  mutate(sequence_to_model = str_sub(sequence, min(sequence_window), max(sequence_window)))
+
+#Add new columns (encoding)
+encoded_sequences = data %>%
+  pull(sequence_to_model) %>%
+  encode_peptide(m = "z_scales")
+
+write.csv(encoded_sequences,"data/encoded_seq_4.csv", row.names = TRUE)
